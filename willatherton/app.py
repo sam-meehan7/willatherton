e9e31958a98c277b621f9a4e4cee13b478f974f1
Sam <sammeehansam@gmail.com>
2024-02-06 23:09:26 +0000

    First commit

---
+++ willatherton/app.py
@@ -0,0 +1,72 @@
+# Import necessary libraries
+from ask_vector_and_supplment_with_ai import answer_question
+import streamlit as st
+from st_click_detector import click_detector
+import random
+
+# Define an array of fun facts
+fun_facts = [
+    "DoneDeal has over 70,000 cars for sale!",
+    "DoneDeal has over 15,000 electric and hybrid cars for sale!"
+]
+
+# Title of the Streamlit app
+st.title("Your Dog Training Assistant")
+
+# Using st.form to create a form
+with st.form(key='my_form'):
+    # Create a text input for the user to enter their question
+    question = st.text_input("How can I help with your dog training?:")
+
+    # Create a submit button within the form
+    submit_button = st.form_submit_button(label='Submit')
+
+
+fun_fact_placeholder = st.empty()
+
+if submit_button:
+    # Select a random fun fact
+    fun_fact = random.choice(fun_facts)
+
+    # Display the fun fact and loading message
+    with fun_fact_placeholder.container():
+        with st.spinner('Fetching your results...'):
+            # Fetch the answer and vector results
+            answer, vector_results = answer_question(question)
+
+    # Clear the status (fun fact and loading message)
+    fun_fact_placeholder.empty()
+
+    # Display the answer
+    st.write(answer)
+
+    # Display source links in a grid format
+    st.subheader("Source Links:")
+
+    # Initialize the HTML content
+    html_content = "<div style='display: flex; flex-wrap: wrap;'>"
+
+    for result in vector_results:
+        # Extract necessary data from the result's metadata
+        title = result.metadata["title"]
+        url = result.metadata["url"]
+        start_time = result.metadata["start"]
+        thumbnail = result.metadata["thumbnail"]
+
+        # Construct the URL with start time appended
+        clickable_url = f"{url}&t={int(start_time)}s"
+
+        # Append each grid item to the HTML content
+        html_content += f"""
+        <div style='flex: 33%; padding: 5px; text-align: center;'>
+            <a href='{clickable_url}' target='_blank'>
+                <img src='{thumbnail}' width='100'>
+                <p>{title}</p>
+            </a>
+        </div>
+        """
+
+    # Close the div
+    html_content += "</div>"
+
+    click_detector(html_content)
---
+++ willatherton/ask_vector_and_supplment_with_ai.py
@@ -0,0 +1,113 @@
+import os
+from pinecone import Pinecone as PineconePincone
+
+from langchain_community.vectorstores import Pinecone as VectorstorePinecone
+from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings
+
+from dotenv import load_dotenv
+import cohere
+
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+
+load_dotenv()
+
+os.environ["COHERE_API_KEY"] = os.getenv("COHERE_API_KEY")
+# init client
+co = cohere.Client(os.environ["COHERE_API_KEY"])
+
+chat = ChatOpenAI(
+    openai_api_key=os.environ["OPENAI_API_KEY"],
+    model='gpt-4-0125-preview',
+    temperature=0.1,
+    max_tokens=3500
+    )
+
+pc = PineconePincone(api_key=os.environ["PINECONE_API_KEY"])
+index = pc.Index("willatherton")
+
+embed_model = OpenAIEmbeddings(model="text-embedding-3-small")
+
+text_field = "text"  # the metadata field that contains our text
+
+# pylint: disable=not-callable
+vectorstore = VectorstorePinecone(index, embed_model.embed_query, text_field)
+
+
+def search_vectorstore(query: str):
+    results = vectorstore.similarity_search(query, k=25)
+
+    # Convert results to format suitable for Cohere reranking
+    documents_to_rerank = [{"text": result.page_content} for result in results]
+
+    # Use Cohere to rerank the documents
+    reranked_response = co.rerank(
+        query=query, documents=documents_to_rerank, model="rerank-english-v2.0"
+    )
+
+    reranked_indices = [result.index for result in reranked_response]
+
+    # Take the top 3 reranked results
+    top_results = [results[i] for i in reranked_indices[:3]]
+
+    return top_results
+
+
+def frame_prompt(query: str, vector_results):
+    # import ipdb;
+    # ipdb.set_trace()
+    # print(vector_results)
+
+    # get the text from the results
+    source_knowledge = "\n\n".join([f"{x.metadata['title']}\n{x.page_content}" for x in vector_results])
+
+    # feed into an augmented prompt
+    framed_prompt = f"""
+
+    # THE USERS QUESTION
+    {query}
+
+    # TOP RESULT
+    {source_knowledge}
+
+    """
+    return framed_prompt
+
+
+def answer_question(question):
+    vector_results = search_vectorstore(question)
+
+    framed_prompt = frame_prompt(question, vector_results)
+
+    template = """
+    # MISSION
+    You are experienced dog trainer named Will. You are super friendly and confident in your knowledge.
+    You have an incredible love for dogs and believe in the power of positive reinforcement.
+    A user will seek advice on a problem they are having with their dog and you will help them solve it.
+
+    # INSTRUCTIONS
+    You will draw on your top results to help answer the users question. These top results are from your own knowledge and experience.
+    You will give the user some advice in a well structured format.
+
+    # RULES
+    You should never make up information. Always use the context given to you.
+    """
+
+    system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+    human_template = "{add_aug_prompt_here}"
+    human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+
+    chat_prompt = ChatPromptTemplate.from_messages(
+        [system_message_prompt, human_message_prompt]
+    )
+
+    result = chat(
+        chat_prompt.format_prompt(
+            add_aug_prompt_here=framed_prompt, text=""
+        ).to_messages()
+    )
+    return [result.content, vector_results]
